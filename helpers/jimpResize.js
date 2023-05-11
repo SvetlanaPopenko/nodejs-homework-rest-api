64a16d0ecdda8b9777529bbbdc8065ff43538223
SvetlanaPopenko <spopenko02@gmail.com>
2023-05-11 16:39:21 +0300

    add fix

--- helpers/jimpResize.js
+++ helpers/jimpResize.js
@@ -3,7 +3,7 @@ const jimp = require("jimp");
 const jimpResize = async (path) => {
   try {
     const image = await jimp.read(path);
-    await image.cover(250, 250);
+    await image.contain(250, 250);
     await image.resize(250, 250);
     await image.writeAsync(path);
   } catch (error) {
