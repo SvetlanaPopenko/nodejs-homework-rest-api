90a1f30fcc1f3cdf2d7e1143b29f382e53ef5655
SvetlanaPopenko <spopenko02@gmail.com>
2023-05-14 16:21:54 +0300

    add hw06

--- models/user.js
+++ models/user.js
@@ -26,7 +26,15 @@ const userSchema = new Schema(
     },
     avatarURL: {
       type: String,
-      required:true,
+      required: true,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
     },
   },
   { versionKey: false, timestamps: true }
@@ -40,6 +48,10 @@ const registerSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
@@ -47,6 +59,7 @@ const loginSchema = Joi.object({
 
 const schemas = {
   registerSchema,
+  emailSchema,
   loginSchema,
 };
 
